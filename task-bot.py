c25bd5f4a33844eafc33f20147b98c70a5ed8a47
Jake Howard <jake.howard@dabapps.com>
2020-02-02 15:55:32 +0000

    Extract milestone processing into method

--- task-bot.py
+++ task-bot.py
@@ -4,38 +4,51 @@ from datetime import datetime
 from math import floor
 
 import requests
-from github import Github
+from github import Github, Milestone
+
+
+def as_percentage(a: float, b: float) -> int:
+    return floor((a / b) * 100)
+
+
+def process_milestone(milestone: Milestone) -> str:
+    now = datetime.now()
+    html_url = milestone._rawData["html_url"]
+    segments = []
+    total_issues = milestone.open_issues + milestone.closed_issues
+    percentage_complete = as_percentage(milestone.closed_issues, total_issues)
+    segments.append(
+        f":heavy_check_mark: {percentage_complete}% completed ({milestone.closed_issues}/{total_issues})"
+    )
+    if milestone.due_on:
+        duration = milestone.due_on - milestone.created_at
+        remaining = milestone.due_on - now
+        time_used = as_percentage(remaining.days, duration.days)
+        segments += [
+            f":date: {milestone.due_on.date().isoformat()}",
+            f":alarm_clock: {time_used}% time used",
+        ]
+    merged_segments = "\n".join(["\t" + segment for segment in segments]) + "\n"
+    return f"<{html_url}|{milestone.title}>\n" + merged_segments
 
 
 def main() -> None:
     github = Github(os.environ.get("GITHUB_TOKEN"))
     repo = github.get_repo(sys.argv[1])
-    messages = []
-    now = datetime.now()
-    for milestone in repo.get_milestones(sort="due_on"):
-        html_url = milestone._rawData["html_url"]
-        segments = []
-        total_issues = milestone.open_issues + milestone.closed_issues
-        percentage_complete = floor((milestone.closed_issues / total_issues) * 100)
-        segments.append(
-            f":heavy_check_mark: {percentage_complete}% completed ({milestone.closed_issues}/{total_issues})"
+    messages = [
+        process_milestone(milestone) for milestone in repo.get_milestones(sort="due_on")
+    ]
+    webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+    if webhook_url:
+        response = requests.post(
+            webhook_url,
+            json={
+                "text": "*Task Status*",
+                "attachments": [{"text": "\n".join(messages)}],
+            },
         )
-        if milestone.due_on:
-            duration = milestone.due_on - milestone.created_at
-            remaining = milestone.due_on - now
-            time_used = floor((remaining.days / duration.days) * 100)
-            segments += [
-                f":date: {milestone.due_on.date().isoformat()}",
-                f":alarm_clock: {time_used}% time used",
-            ]
-        merged_segments = "\n".join(["\t" + segment for segment in segments]) + "\n"
-        messages.append(f"<{html_url}|{milestone.title}>\n" + merged_segments)
+        response.raise_for_status()
     print("\n".join(messages))
-    response = requests.post(
-        os.environ["SLACK_WEBHOOK_URL"],
-        json={"text": "*Task Status*", "attachments": [{"text": "\n".join(messages)}]},
-    )
-    response.raise_for_status()
 
 
 if __name__ == "__main__":
