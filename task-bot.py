88583dee5274e761e95b15d4ae89b1cc4fc920ab
Jake Howard <jake.howard@dabapps.com>
2020-02-02 14:32:29 +0000

    Post message to slack

--- task-bot.py
+++ task-bot.py
@@ -2,6 +2,8 @@ from github import Github
 import sys
 from math import floor
 from datetime import datetime
+import requests
+import os
 
 def main():
     github = Github()
@@ -10,14 +12,22 @@ def main():
     now = datetime.now()
     for milestone in repo.get_milestones():
         percentage_complete = round((milestone.closed_issues / (milestone.open_issues + milestone.closed_issues)) * 100)
-        message = f"{milestone.title}: {percentage_complete}% complete."
+        message = f"*{milestone.title}*: {percentage_complete}% complete."
         if milestone.due_on:
             duration = milestone.due_on - milestone.created_at
             remaining = milestone.due_on - now
             time_used = round((remaining.days / duration.days) * 100)
             message += f" Due {milestone.due_on.date().isoformat()}. {remaining.days} days remaining. {time_used}% time used."
         messages.append(message)
+    messages = sorted(messages)
     print("\n".join(messages))
+    response = requests.post(
+        os.environ['SLACK_WEBHOOK_URL'],
+        json={
+            "text": "*Task Status*",
+            "attachments": [{"text": "\n".join(messages)}]
+        }
+    )
 
 
 if __name__ == '__main__':
