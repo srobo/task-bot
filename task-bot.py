d71e75a18d26c3190161cd316c2629a45d56451d
Jake Howard <jake.howard@dabapps.com>
2020-02-02 14:19:28 +0000

    Print a more detailed message

--- task-bot.py
+++ task-bot.py
@@ -1,13 +1,24 @@
 from github import Github
 import sys
 from math import floor
+from datetime import datetime
 
 def main():
     github = Github()
     repo = github.get_repo(sys.argv[1])
+    messages = []
+    now = datetime.now()
     for milestone in repo.get_milestones():
-        percentage_complete = (milestone.closed_issues / (milestone.open_issues + milestone.closed_issues)) * 100
-        print(milestone.title, floor(percentage_complete), milestone.open_issues, milestone.closed_issues)
+        percentage_complete = round((milestone.closed_issues / (milestone.open_issues + milestone.closed_issues)) * 100)
+        message = f"{milestone.title}: {percentage_complete}% complete."
+        if milestone.due_on:
+            duration = milestone.due_on - milestone.created_at
+            remaining = milestone.due_on - now
+            time_used = round((remaining.days / duration.days) * 100)
+            message += f" Due {milestone.due_on.date().isoformat()}. {remaining.days} days remaining. {time_used}% time used."
+        messages.append(message)
+    print("\n".join(messages))
+
 
 if __name__ == '__main__':
     main()
